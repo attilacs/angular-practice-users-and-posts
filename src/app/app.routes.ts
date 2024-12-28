9dc2da015e5bddc4cadcb05617038be732a14d0e
attilacs <csanyiattila23@gmail.com>
2024-12-28 14:42:55 +0100

    Implement simple not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsersComponent } from './components/users/users.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'users', title: 'Users', component: UsersComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', title: 'Not Found', component: PageNotFoundComponent },
 ];

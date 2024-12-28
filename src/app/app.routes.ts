1f21534e95ae02631531a8b4fa66b8eea014c7ca
attilacs <csanyiattila23@gmail.com>
2024-12-28 14:41:31 +0100

    Generate users component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'users', title: 'Users', component: UsersComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];

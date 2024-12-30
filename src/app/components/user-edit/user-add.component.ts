7239a505cc0cca0260e07162228eb158603bfd71
attilacs <csanyiattila23@gmail.com>
2024-12-30 15:08:11 +0100

    Add update user method to component

--- src/app/components/user-edit/user-add.component.ts
+++ src/app/components/user-edit/user-add.component.ts
@@ -1,9 +1,14 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../../services/user/user.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { catchError, EMPTY, map, Observable, of, switchMap } from 'rxjs';
+import { catchError, map, Observable, of, Subscription, switchMap } from 'rxjs';
 import { User } from '../../interfaces/user';
+
+interface UserForm {
+  name: FormControl<string | null>;
+  email: FormControl<string | null>;
+}
 
 @Component({
   selector: 'app-user-edit',
@@ -17,18 +22,40 @@ export class UserEditComponent implements OnInit {
   private service = inject(UserService);
   private router = inject(Router);
   private route = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+  private initSubscription: Subscription | undefined;
+  private saveSubscription: Subscription | undefined;
   isError = false;
-  user$: Observable<User | null> = EMPTY;
+  userId: string | null = null;
+  form: FormGroup<UserForm>;
+
+  constructor() {
+    this.destroyRef.onDestroy(() => {
+      this.initSubscription?.unsubscribe();
+      this.saveSubscription?.unsubscribe();
+    });
+
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    })
+  }
 
   ngOnInit(): void {
-    this.user$ = this.route.paramMap.pipe(
+    this.initSubscription = this.route.paramMap.pipe(
       switchMap((param) => {
         const id = param.get('id');
-        if (typeof id === 'string' && id !== 'add') {
+        if (id) {
           return this.service.getUserById(id).pipe(
             map((user) => {
               if (user.length) {
-                return user[0];
+                const userData = user[0];
+                if (!userData) {
+                  return null;
+                }
+                this.userId = userData.id;
+                this.initUserData(userData);
+                return userData;
               }
               return null;
             }),
@@ -40,21 +67,29 @@ export class UserEditComponent implements OnInit {
         }
         return of(null);
       }),
-    );
+    ).subscribe();
   }
 
-  form = new FormGroup({
-    name: new FormControl("", [Validators.required]),
-    email: new FormControl("", [Validators.required, Validators.email]),
-  });
+  private initUserData(initial?: User) {
+    this.form.setValue({
+      name: initial?.name ?? '',
+      email: initial?.email ?? '',
+    })
+  }
 
-  addUser(): void {
-    const name = this.form.controls.name.value;
-    const email = this.form.controls.email.value;
+  saveUser(): void {
+    if (!this.form?.valid) {
+      return;
+    }
+    const { name, email } = this.form.value;
     if (!name || !email) {
       throw new Error('Invalid input data!');
     }
-    this.service.addUser({ name, email }).subscribe({
+    const apiCall: Observable<User> = this.userId
+      ? this.service.updateUser(this.userId, { name, email })
+      : this.service.addUser({ name, email });
+
+    this.saveSubscription = apiCall.subscribe({
       next: () => {
         this.router.navigate(["/users"]);
       },

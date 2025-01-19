5d8418a2e0c0e79f89e8de1ecc60a3b897997f75
attilacs <csanyiattila23@gmail.com>
2025-01-19 17:57:55 +0100

    Generate posts-edit component

---
+++ src/app/components/posts-edit/posts-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-edit',
+  imports: [],
+  templateUrl: './posts-edit.component.html',
+  styleUrl: './posts-edit.component.css',
+})
+export class PostsEditComponent {
+}
